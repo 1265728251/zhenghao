4bc06d4134266342b208d4498baf3eff40f8e53b
yangzhi <1265728251@qq.com>
2023-02-08 11:16:48 +0800

    解析学历信息

--- src/main/java/com/xdap/zhenghao/demo/controller/userInforController.java
+++ src/main/java/com/xdap/zhenghao/demo/controller/userInforController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xdap.zhenghao.demo.entity.UserInFor;
 import com.xdap.zhenghao.demo.service.IGetEmployeeInformationService;
+import com.xdap.zhenghao.demo.utils.highestEdUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,8 @@ public class userInforController {
 
     @Autowired
     private IGetEmployeeInformationService iGetEmployeeInformationService;
+    @Autowired
+    private highestEdUtils highestEdUtils;
 
     @ResponseBody
     @RequestMapping(value = "/save", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
@@ -37,12 +40,15 @@ public class userInforController {
         System.out.println("请求的数据"+jsonParam.toJSONString());
         JSONObject jsonObject =new JSONObject();
         ObjectMapper objectMapper = new ObjectMapper();
+
         try {
             UserInFor user = iGetEmployeeInformationService.getUserInFor("+86-"+jsonParam.get("mobile").toString());
             // 将实体类转换为json
-            String s = objectMapper.writeValueAsString(user);
+            String s = objectMapper.writeValueAsString(highestEdUtils.highest(user));
             System.out.println("请求返回的数据是："+s);
             jsonObject = (JSONObject) JSON.parse(s);
+
+            jsonObject.put("data",jsonObject.toJSONString());
 
         }catch (Exception e) {
             e.printStackTrace();
